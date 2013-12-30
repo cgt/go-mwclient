b88e49cada4875b6d15b950aef8d9385aaa90f97
Christoffer G. Thomsen <chris@cgt.name>
2013-12-30 03:26:03 +0100

    Added functionality for getting MediaWiki tokens.
    Added the GetToken method and a map[string]string to the Wiki type which
    acts as a container for tokens.
    
    Added field Tokens to Wiki of type map[string]string for storing tokens.
    Added GetToken method for retrieving tokens and storing them in
    Wiki.Tokens.

--- core.go
+++ core.go
@@ -21,6 +21,7 @@ type Wiki struct {
 	cjar              *cookiejar.Jar
 	ApiUrl            *url.URL
 	format, UserAgent string
+	Tokens            map[string]string
 }
 
 // NewWiki returns an initialized Wiki object. If the provided API url is an
@@ -38,6 +39,7 @@ func NewWiki(inUrl string) *Wiki {
 		apiurl,
 		"json",
 		DefaultUserAgent,
+		map[string]string{},
 	}
 }
 
@@ -186,4 +188,46 @@ func (w *Wiki) Login(username, password string) error {
 func (w *Wiki) Logout() bool {
 	w.Get(url.Values{"action": {"logout"}})
 	return true
+}
+
+// GetToken returns a specified token (and an error if this is not possible).
+// If the token is not already available in the Wiki.Tokens map,
+// it will attempt to retrieve it via the API.
+// tokenName should be "edit" (or whatever), not "edittoken".
+func (w *Wiki) GetToken(tokenName string) (string, error) {
+	if _, ok := w.Tokens[tokenName]; ok {
+		log.Println("Got from map")
+		return w.Tokens[tokenName], nil
+	}
+
+	parameters := url.Values{
+		"action": {"tokens"},
+		"type":   {tokenName},
+	}
+
+	resp, err, apiok := ErrorCheck(w.Get(parameters))
+	if err != nil {
+		return "", err
+	}
+	if !apiok {
+		// Check for errors
+		if err, ok := resp.CheckGet("error"); ok {
+			newError := fmt.Errorf("%s: %s", err.Get("code").MustString(), err.Get("info").MustString())
+			return "", newError
+		}
+
+		// Check for warnings
+		if warnings, ok := resp.CheckGet("warnings"); ok {
+			newError := fmt.Errorf(warnings.GetPath("tokens", "*").MustString())
+			return "", newError
+		}
+	}
+
+	token, err := resp.GetPath("tokens", tokenName+"token").String()
+	if err != nil {
+		// This really shouldn't happen.
+		return "", fmt.Errorf("Error occured while converting token to string: %s", err)
+	}
+	w.Tokens[tokenName] = token
+	return token, nil
 }
