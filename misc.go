e238ba692c29079bacca68199b517448636f4045
Christoffer G. Thomsen <chris@cgt.name>
2014-02-09 13:14:53 +0100

    Move helpers.go content to misc.go
    There's really no reason to have two separate files for miscellaneous
    stuff. Also, rename helpers_test.go to misc_test.go
    
    Clean up the loop GetPageID and improve safety. If the query, pages
    field cannot be asserted to a map, return an error instead of failing
    stupidly.

--- misc.go
+++ misc.go
@@ -1,6 +1,10 @@
 package mwclient
 
-import "net/url"
+import (
+	"bytes"
+	"net/url"
+	"sort"
+)
 
 // GetPageID gets the pageid of a page specified by its name.
 func (w *Client) GetPageID(pageName string) (string, error) {
@@ -15,10 +19,51 @@ func (w *Client) GetPageID(pageName string) (string, error) {
 		return "", err
 	}
 
+	pageMap, err := resp.GetPath("query", "pages").Map()
+	if err != nil {
+		return "", err
+	}
+
 	var id string
-	for k, _ := range resp.GetPath("query", "pages").MustMap() {
+	for k := range pageMap {
 		// There should only be one item in the map.
 		id = k
 	}
 	return id, nil
+}
+
+// URLEncode is a slightly modified version of Values.Encode() from net/url.
+// It encodes url.Values into URL encoded form, sorted by key, with the exception
+// of the key "token", which will be appended to the end instead of being subject
+// to regular sorting. This is done because that's what the MediaWiki API wants.
+func urlEncode(v url.Values) string {
+	if v == nil {
+		return ""
+	}
+	var buf bytes.Buffer
+	keys := make([]string, 0, len(v))
+	for k := range v {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	token := false
+	for _, k := range keys {
+		if k == "token" {
+			token = true
+			continue
+		}
+		vs := v[k]
+		prefix := url.QueryEscape(k) + "="
+		for _, v := range vs {
+			if buf.Len() > 0 {
+				buf.WriteByte('&')
+			}
+			buf.WriteString(prefix)
+			buf.WriteString(url.QueryEscape(v))
+		}
+	}
+	if token {
+		buf.WriteString("&" + url.QueryEscape("token") + "=" + url.QueryEscape(v["token"][0]))
+	}
+	return buf.String()
 }
