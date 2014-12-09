2ce8de46227f4fe3034f59e574a11d30773a8171
Christoffer G. Thomsen <chris@cgt.name>
2014-12-09 12:54:06 +0100

    Add doc comments for Dump/LoadCookies

--- misc.go
+++ misc.go
@@ -8,10 +8,12 @@ import (
 	"sort"
 )
 
+// DumpCookies exports the cookies stored in the client.
 func (w *Client) DumpCookies() []*http.Cookie {
 	return w.cjar.Cookies(w.APIURL)
 }
 
+// LoadCookies imports cookies into the client.
 func (w *Client) LoadCookies(cookies []*http.Cookie) {
 	w.cjar.SetCookies(w.APIURL, cookies)
 }
