a100ce6824bd883e371201f6828291ca66c90cf1
Christoffer G. Thomsen <chris@cgt.name>
2015-01-30 14:38:10 +0100

    Implement support for query continuations
    The Query type provides a nice interface for dealing with queries. It is
    intended to be used much like the bufio.Scanner type from the standard
    library. I.e., instantiate, loop, check for errors. More details
    available in the doc comments.

---
+++ query.go
@@ -0,0 +1,103 @@
+package mwclient
+
+import (
+	"fmt"
+
+	"cgt.name/pkg/go-mwclient/params"
+	"github.com/bitly/go-simplejson"
+)
+
+// Query provides a simple interface to deal with query continuations.
+// A Query should be instantiated through the NewQuery method on the Client type.
+// Once you have instantiated a Query, call the Next method to retrieve the
+// first set of results from the API.
+// If Next returns false, then either you have received all the results for the
+// query or an error occured. If an error occurs, it will be available through
+// the Err method.
+// If Next returns true, then there are more results to be retrieved and another
+// call to Next will retrieve the next results.
+//
+// The following example will retrieve all the pages that are in the category
+// "Soap":
+//	p := params.Values{
+//		"list": "categorymembers",
+//		"cmtitle": "Category:Soap",
+//	}
+//	q := w.NewQuery(p) // w being an instantiated Client
+//	for q.Next() {
+//		fmt.Println(q.Resp())
+//	}
+//	if q.Err() != nil {
+//		// handle the error
+//	}
+// See https://www.mediawiki.org/wiki/API:Query for more details on how to
+// query the MediaWiki API.
+type Query struct {
+	mwc    *Client
+	params params.Values
+	resp   *simplejson.Json
+	err    error
+}
+
+// Err returns the first error encountered by the Next method.
+func (q Query) Err() error {
+	return q.err
+}
+
+// Resp returns the API response retrieved by the Next method.
+func (q Query) Resp() *simplejson.Json {
+	return q.resp
+}
+
+// NewQuery instantiates a new query with the given parameters.
+func (w *Client) NewQuery(p params.Values) *Query {
+	p.Set("action", "query")
+	p.Set("continue", "")
+
+	return &Query{
+		mwc:    w,
+		params: p,
+		resp:   nil,
+		err:    nil,
+	}
+}
+
+// Next retrieves the next set of results from the API and makes them available
+// through the Resp method. Next returns true if there are more results to be
+// retrieved, or false if all the results have been retrieved or if an error
+// occurs.
+func (q *Query) Next() (done bool) {
+	firstCall := false
+	if q.resp == nil {
+		// first call to Next
+		firstCall = true
+		q.resp, q.err = q.mwc.Get(q.params)
+	}
+
+	cont, ok := q.resp.CheckGet("continue")
+	if !ok {
+		if firstCall {
+			return true
+		}
+		return false
+	}
+	cm, err := cont.Map()
+	if err != nil {
+		q.err = fmt.Errorf("response processing error: %v", err)
+		return false
+	}
+	for k, v := range cm {
+		value, ok := v.(string)
+		if !ok {
+			q.err = fmt.Errorf("response processing error: %v", err)
+			return false
+		}
+		q.params.Set(k, value)
+	}
+
+	q.resp, q.err = q.mwc.Get(q.params)
+	if q.err != nil {
+		return false
+	}
+	return true
+}
